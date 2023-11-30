69cedd0cb88cba7cbca7639c3e74eae0eee03783
Umair <umairwaraich2019@gmail.com>
2023-11-30 12:04:02 +0500

    full setup configuration

--- src/app.js
+++ src/app.js
@@ -10,18 +10,20 @@ app.use(
     credentials: true,
   })
 );
-
 app.use(express.json({ limit: "16kb" }));
-
 app.use(
   express.urlencoded({
     extended: true,
     limit: "16kb",
   })
 );
-
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+// import route
+import userRouter from "./routes/user.routes.js";
+
+// routes
+app.use("/api/v1/users", userRouter);
 
 export { app };
