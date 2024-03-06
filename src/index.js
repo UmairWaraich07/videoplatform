7dd250056d2c38069a3ddb4921b8ddb793116fce
Umair <umairwaraich2019@gmail.com>
2024-03-06 21:07:35 +0500

    add utils files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 import connectToDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env",
 });
 
-connectToDB();
+connectToDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`App is listening at PORT : ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(`MongoDB connection Failed, ${err}`);
+    });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (fn) => {
+    (req, res, next) => {
+        Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+    };
+};
+
+export { asyncHandler };
+
+// export const asyncHandler2 = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(400).json({
+//             message: "Internal Server Error",
+//             success: false,
+//         });
+//     }
+// };
