85ab5bc6a02941b939261ba33561acf58e33bedd
Umair <umairwaraich2019@gmail.com>
2024-03-06 17:57:46 +0500

    connected to DB

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectToDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        );
+        console.log(
+            `DB connected : HOST : ${connectionInstance.connection.host}`
+        );
+    } catch (error) {
+        console.log(`DB connection ERROR, ${error}`);
+        process.exit(1);
+    }
+};
+
+export default connectToDB;
