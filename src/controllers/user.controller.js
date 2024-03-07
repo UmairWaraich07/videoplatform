ad9294ef4419b1387aca42a4333228e4e80516b2
Umair <umairwaraich2019@gmail.com>
2024-03-07 16:00:01 +0500

    added the user register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,60 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "OK",
+    const { fullname, username, email, password } = req.body;
+
+    if (
+        [fullname, username, email, password].some(
+            (field) => field?.trim() === ""
+        )
+    ) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const avatarLocalPath = req.files?.avatar && req.files?.avatar[0]?.path;
+    const coverImageLocalPath =
+        req.files?.coverImage && req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required");
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{ email }, { username }],
     });
+
+    if (existedUser) {
+        throw new ApiError(
+            400,
+            "User with this email or username already exists!"
+        );
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    if (!avatar)
+        throw new ApiError(400, "Failed to upload image on Cloudinary!");
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    const createdUser = await User.create({
+        fullname,
+        username: username.toLowerCase(),
+        email,
+        password,
+        avatar: avatar.url,
+        coverImage: coverImage ? coverImage.url : "",
+    });
+
+    const response = await User.findById(createdUser._id).select(
+        "-password -refreshToken"
+    );
+
+    if (!response) throw new ApiError(500, "Failed to register the User");
+
+    return response;
 });
 
 export { registerUser };
